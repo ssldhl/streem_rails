a5663df839c2c5451d6f691b01805f1e67009709
sushil-bigfuture <sushil@bigfuture.com.au>
2022-02-20 20:59:58 +0545

    Allow cors

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,5 +1,13 @@
 class ResultsController < ApplicationController
+  before_action :allow_cors
+
   def index
     @news_result = News.new(params).aggregate
+  end
+
+  private
+
+  def allow_cors
+    headers['Access-Control-Allow-Origin'] = '*'
   end
 end
